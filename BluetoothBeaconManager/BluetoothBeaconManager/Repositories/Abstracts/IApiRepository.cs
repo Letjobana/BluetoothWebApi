688f0c229e85d131491c77b121914e21bf40d465
Letjobana <suzanm@gerbertech.co.za>
2022-06-09 10:12:19 +0200

    Added IApiRepository

---
+++ BluetoothBeaconManager/BluetoothBeaconManager/Repositories/Abstracts/IApiRepository.cs
@@ -0,0 +1,17 @@
+﻿using Geotab.Checkmate;
+using Geotab.Checkmate.ObjectModel;
+using System.Collections.Generic;
+
+namespace BluetoothBeaconManager.Repositories.Abstracts
+{
+    public interface IApiRepository
+    {
+        bool Aunthenticate(API api);
+        User GetUserByUseName(string name);
+        IEnumerable<Device> GetActiveDevices();
+        IEnumerable<User> GetUsers();
+        Device GetDeviceById(string id);
+        Group GetGroupByName(string groupName);
+        List<Group> GetGroups();
+    }
+}
