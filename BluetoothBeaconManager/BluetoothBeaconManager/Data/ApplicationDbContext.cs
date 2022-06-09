11263c2263411ba1d5a0fc59a9b1ab45a4b1748c
Letjobana <suzanm@gerbertech.co.za>
2022-06-09 09:54:11 +0200

    Added a new class

---
+++ BluetoothBeaconManager/BluetoothBeaconManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BluetoothBeaconManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BluetoothBeaconManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<BluetoothStatusData> BluetoothStatusDatas { get; set; }
+    }
+}
