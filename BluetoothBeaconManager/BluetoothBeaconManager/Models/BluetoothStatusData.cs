5e365f234da4c0e1ae3172be4f86562085253ae7
Letjobana <suzanm@gerbertech.co.za>
2022-06-09 09:49:10 +0200

    Added BluetoothStatusData class

---
+++ BluetoothBeaconManager/BluetoothBeaconManager/Models/BluetoothStatusData.cs
@@ -0,0 +1,12 @@
+﻿namespace BluetoothBeaconManager.Models
+{
+    public class BluetoothStatusData
+    {
+        public string DiagnosticType { get; set; }
+        public string Device { get; set; }
+        public string Date { get; set; }
+        public string BeaconName { get; set; }
+        public string Address { get; set; }
+        public string Value { get; set; }
+    }
+}
